1bc535039f928f0621f19c77497593c2e4ccafb1
Richard Habeeb <richard.habeeb@gmail.com>
2017-02-24 12:50:14 -0500

    Added more SoC devices for the am335x.

--- include/plat/am335x/plat/machine/devices.h
+++ include/plat/am335x/plat/machine/devices.h
@@ -21,17 +21,44 @@
 
 
 /* Other devices on the SoC. */
-#define INTC_PADDR      0x48200000
-#define UART0_PADDR     0x44E09000
-#define DMTIMER0_PADDR  0x44E05000
-#define DMTIMER2_PADDR  0x48040000
-#define DMTIMER3_PADDR  0x48042000
-#define DMTIMER4_PADDR  0x48044000
-#define DMTIMER5_PADDR  0x48046000
-#define DMTIMER6_PADDR  0x48048000
-#define DMTIMER7_PADDR  0x4804A000
-#define WDT1_PADDR      0x44e35000
-#define CMPER_PADDR     0x44e00000
-
+#define INTC_PADDR      0x48200000 /* 4 KB */
+#define UART0_PADDR     0x44E09000 /* 4 KB */
+#define UART1_PADDR     0x48022000 /* 4 KB */
+#define UART2_PADDR     0x48024000 /* 4 KB */
+#define UART3_PADDR     0x481A6000 /* 4 KB */
+#define UART4_PADDR     0x481A8000 /* 4 KB */
+#define UART5_PADDR     0x481AA000 /* 4 KB */
+#define DMTIMER0_PADDR  0x44E05000 /* 4 KB */
+#define DMTIMER1_PADDR  0x44E31000 /* 4 KB */
+#define DMTIMER2_PADDR  0x48040000 /* 4 KB */
+#define DMTIMER3_PADDR  0x48042000 /* 4 KB */
+#define DMTIMER4_PADDR  0x48044000 /* 4 KB */
+#define DMTIMER5_PADDR  0x48046000 /* 4 KB */
+#define DMTIMER6_PADDR  0x48048000 /* 4 KB */
+#define DMTIMER7_PADDR  0x4804A000 /* 4 KB */
+#define WDT0_PADDR      0x44E33000 /* 4 KB */
+#define WDT1_PADDR      0x44E35000 /* 4 KB */
+#define GPIO0_PADDR     0x44E07000 /* 4 KB */
+#define GPIO1_PADDR     0x4804C000 /* 4 KB */
+#define GPIO2_PADDR     0x481AC000 /* 4 KB */
+#define GPIO3_PADDR     0x481AE000 /* 4 KB */
+#define USBSS_PADDR     0x47400000 /* 20 KB */
+#define SPI0_PADDR      0x48030000 /* 4 KB */
+#define SPI1_PADDR      0x481A0000 /* 4 KB */
+#define MMCHS0_PADDR    0x48060000 /* 4 KB */
+#define MMCHS1_PADDR    0x481D8000 /* 4 KB */
+#define MMCHS2_PADDR    0x47810000 /* 64 KB */
+#define GPMC0_PADDR     0x50000000 /* 16 MB */
+#define ELM0_PADDR      0x48080000 /* 64 KB */
+#define I2C0_PADDR      0x44E0B000 /* 4 KB */
+#define I2C1_PADDR      0x4802A000 /* 4 KB */
+#define I2C2_PADDR      0x4819C000 /* 4 KB */
+#define CMPER_PADDR     0x44E00000 /* 1 KB */
+#define CONTROL_PADDR   0x44E10000 /* 128 KB */
+#define EDMA30CC0_PADDR 0x49000000 /* 1 MB */
+#define DCAN0_PADDR     0x481CC000 /* 8 KB */
+#define DCAN1_PADDR     0x481D0000 /* 8 KB */
+#define LCDC0_PADDR     0x4830E000 /* 4 KB */
+#define ADCTSC0_PADDR   0x44E0D000 /* 4 MB */
 
 #endif
